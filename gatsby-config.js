6b96058fab61ce029e77a348b5066765a72b721a
bugaka <47617867+bugaka@users.noreply.github.com>
2022-07-21 23:57:43 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `@bugaka/gatsby-theme-minimal-blog`,
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
         navigation: [
