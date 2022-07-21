415426ed9994fce00c71a01f1fd577e0fb1ec826
bugaka <47617867+bugaka@users.noreply.github.com>
2022-07-21 22:37:55 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,37 +49,30 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     {
-       {
-      resolve: `gatsby-plugin-sitemap`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        host: 'https://www.vukjovin.com',
-        query: `
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
           {
-           site {
-             siteMetadata {
-                siteUrl
-              }
-            }
-
-            allSitePage {
-              edges {
-                node {
-                  path
-                }
-              }
-            }
-        }`,
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map(edge => {
-            return {
-              url: site.siteMetadata.siteUrl + edge.node.path,
-              changefreq: `daily`,
-              priority: 0.7,
-            }
-          })
-      }
-    }
-      },
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      }, 
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
