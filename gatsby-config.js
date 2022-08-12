49b0251ae08b712ae6c4bd19f64aa23a13392ccc
bugaka <47617867+bugaka@users.noreply.github.com>
2022-08-12 16:39:49 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,43 +70,5 @@ module.exports = {
           }
         `,
       },
-    },
-   {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Vuk Jovin's blog and bio site`,
-        short_name: `Vuk Jovin's site`,
-        description: `Content about content writing services and SEO`,
-        start_url: `/`,
-        background_color: `#fff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      }, 
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-netlify`,
-    shouldAnalyseBundle && {
-      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-      options: {
-        analyzerMode: `static`,
-        reportFilename: `_bundle.html`,
-        openAnalyzer: false,
-      },
-    },
-  ].filter(Boolean),
-}
+    }
+
