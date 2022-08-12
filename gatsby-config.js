4134cae30d583f322980319332feeee69bded95d
bugaka <47617867+bugaka@users.noreply.github.com>
2022-08-12 16:34:15 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,3 +72,17 @@ module.exports = {
       },
     },
    
+    {
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
